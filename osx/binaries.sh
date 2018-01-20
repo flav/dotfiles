7509a1cfd3b7f7597f19b02fff2eb24f937c3832
Flavio daCosta <flav@binaryservice.com>
2018-01-19 19:02:38 -0500

    ssh-askpass for when `ssh-add -c`
    Pops a dialog to confirm use of key

--- osx/binaries.sh
+++ osx/binaries.sh
@@ -34,6 +34,9 @@ binaries=(
 # Install the binaries
 brew install ${binaries[@]}
 
+brew tap theseal/ssh-askpass
+brew install ssh-askpass
+
 # Remove outdated versions from the cellar
 brew cleanup
 
