cbea5e25c25bc13b5cb0696d9784f461577e73ec
Flavio daCosta <flav@binaryservice.com>
2019-09-29 10:32:33 -0400

    Remove stow dep - it’s frequently not installed
    And can’t always install it either.

--- Makefile
+++ Makefile
@@ -1,18 +1,10 @@
-default: install
+default: link-dotfiles
 
-dependencies: install-homebrew
-	@command -v stow >/dev/null 2>&1 || brew install stow 2>/dev/null || sudo apt-get install -y stow 2>/dev/null || sudo yum install -y stow 2>/dev/null || { echo >&2 "Please install GNU stow"; exit 1; }
+link-dotfiles:
+	sh dotfiles/install.sh
 
 install-homebrew:
 	sh osx/install-homebrew.sh
 
-submodules:
-	git submodule update --init
-
-stowconfig:
-	sh stow/index.sh
-
-install: dependencies submodules stowconfig
-
-mac: install
+mac: install-homebrew
 	sh osx/index.sh
---
+++ dotfiles/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $DIR
+set -e
+
+function makeLinks {
+    DEST=$1
+    SOURCE_DIR=$DIR/$2
+    for f in `ls -A $SOURCE_DIR`; do
+        SOURCE=$SOURCE_DIR/$f
+        TARGET=$DEST/$f
+
+        echo "Linking ${DEST}/${f}"
+
+        # Note explicit *link* (-L) and *exists* (-e) checks as a broken links
+        # fail the (-e) check.
+        if [ -L $TARGET ]; then
+            echo " Link exists - removing " $TARGET
+            rm $TARGET
+        elif [ -e $TARGET ]; then
+            BACKUPFILE="${TARGET}.`date +"%Y%m%d_%H%M%S"`.bak"
+            echo " File exists - making backup " $BACKUPFILE
+            mv $TARGET $BACKUPFILE
+        fi
+
+        ln -s $SOURCE $TARGET
+    done
+}
+
+mkdir ~/bin 2>/dev/null || true
+
+makeLinks $HOME ag
+makeLinks $HOME bash
+makeLinks $HOME git
+makeLinks $HOME screen
+makeLinks $HOME tmux
+makeLinks $HOME vim
